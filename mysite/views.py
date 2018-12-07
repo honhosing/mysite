a2095e97766376609cfb9147d9870314bd641bb2
Peter Hon <hanhaoxing@gmail.com>
2018-12-07 17:32:13 +0800

    front end updated

--- mysite/views.py
+++ mysite/views.py
@@ -7,6 +7,8 @@ from django.core.cache import cache
 from read_statistics.utils import get_seven_days_read_data, get_today_hot_data
 from article.models import Article
 
+from article.views import get_article_list_common_data
+
 
 def get_7_days_hot_articles():
     today = timezone.now().date()
@@ -46,5 +48,14 @@ def home(request):
     # context['yesterday_hot_data'] = get_yesterday_hot_data(articel_content_type)
     context['hot_articles_for_7_days'] = hot_articles_for_7_days
     # context['hot_articles_for_7_days'] = get_7_days_hot_articles()
+
+    article_list = Article.objects.order_by('-created_time')
+    context['newest_article'] = article_list[0]
+    context['newest_article_author'] = article_list[0].author.get_nickname_or_username()
+    context['second_article'] = article_list[1]
+    context['second_article_author'] = article_list[1].author.get_nickname_or_username()
+    context['third_article'] = article_list[2]
+    context['third_article_author'] = article_list[2].author.get_nickname_or_username()
+
     return render(request, 'home.html', context)
 
